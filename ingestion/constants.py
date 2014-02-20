dc9ed27fc7f35ac47753199d36d794f94c9b3789
Milan Novacek <nospam@dream.(none)>
2014-02-20 18:34:27 +0100

    Add missing constants file.

---
+++ ingestion/constants.py
@@ -0,0 +1,22 @@
+############################################################
+#  Project: DREAM
+#  Module:  Task 5 ODA Ingestion Engine 
+#  Contribution: Milan Novacek   (CVC)
+#
+#    (c) 2014 Siemens Convergence Creators s.r.o., Prague
+#    Licensed under the 'DREAM ODA Ingestion Engine Open License'
+#     (see the file 'LICENSE' in the top-level directory)
+#
+#  Misc. constants for the ingestion engine.
+#
+############################################################
+
+# set to 0 for no debugging
+IE_DEBUG = 2
+
+NCN_ID_LEN         = 96
+SC_NAME_LEN        = 256
+SC_DESCRIPTION_LEN = 2048
+SC_DSRC_LEN        = 1024
+PROD_ERROR_LEN     = 2048
+
